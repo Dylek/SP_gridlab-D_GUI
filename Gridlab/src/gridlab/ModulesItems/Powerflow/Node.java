0c4f69a2f65ef45f1da7dda0717caf8d240f8ff2
Pavl0 <pawelogorzaly@gmail.com>
2016-04-16 22:33:10 +0200

    Powerflow Classes #2

---
+++ Gridlab/src/gridlab/ModulesItems/Powerflow/Node.java
@@ -0,0 +1,54 @@
+package gridlab.ModulesItems.Powerflow;
+
+import gridlab.ModulesItems.Property;
+
+import java.util.Vector;
+
+/**
+ * Created by Pavlo on 16.04.2016.
+ */
+public class Node {
+    private Vector<Property> properties;
+    public Node(){
+        properties=new Vector<Property>();
+        properties.add(new Property("name", "", ""));
+        properties.add(new Property("phases", "", ""));
+        properties.add(new Property("nominal_voltage", "7200.0", "V"));
+        properties.add(new Property("voltage_A", "7200.0+0d", "V"));
+        properties.add(new Property("voltage_B", "7200.0-120.0d", "V"));
+        properties.add(new Property("voltage_C", "7200.0+120.0d", "V"));
+        properties.add(new Property("voltage_AB", "", "V"));
+        properties.add(new Property("voltage_BC", "", "V"));
+        properties.add(new Property("voltage_CA", "", "V"));
+        properties.add(new Property("current_A", "", "A"));
+        properties.add(new Property("current_B", "", "A"));
+        properties.add(new Property("current_C", "", "A"));
+        properties.add(new Property("power_A", "", ""));
+        properties.add(new Property("power_B", "", ""));
+        properties.add(new Property("power_C", "", ""));
+        properties.add(new Property("shunt_A", "", "mhos"));
+        properties.add(new Property("shunt_B", "", "mhos"));
+        properties.add(new Property("shunt_C", "", "mhos"));
+        properties.add(new Property("bustype", "PQ", "")); //PQ, PV, SWING
+        properties.add(new Property("maximum_voltage_error", "", "V"));
+        properties.add(new Property("busflags", "", ""));
+        properties.add(new Property("reference_bus", "", ""));
+        properties.add(new Property("mean_repair_time", "", "s"));
+    }
+
+    public void SetProperty(Vector<Property> wektor){
+        properties=wektor;
+    }
+    public Vector<Property> GetProperties(){
+        return properties;
+    }
+    public String ToGLM(){
+        String s="";
+        s="object node { \n";
+        for (Property p: properties){
+            if(p.GetName()!=null)s+=p.GetName()+"    "+p.GetValue()+"    "+p.GetUnit()+";\n";
+        }
+        s+="} \n";
+        return s;
+    }
+}
--- Gridlab/src/gridlab/ModulesItems/Powerflow/OverheadLineConductor.java
+++ Gridlab/src/gridlab/ModulesItems/Powerflow/OverheadLineConductor.java
@@ -5,26 +5,22 @@ import gridlab.ModulesItems.Property;
 import java.util.Vector;
 
 /**
- * Created by Dylek on 2016-04-16.
+ * Created by Pavlo on 16.04.2016.
  */
-public class TriplexLineConductor {
+public class OverheadLineConductor {
     private Vector<Property> properties;
-    public TriplexLineConductor(){
+    public OverheadLineConductor(){
         properties=new Vector<Property>();
-        properties.add(new Property("name", "",""));
-        properties.add(new Property("geometric_mean_radius", "", "feet"));
+        properties.add(new Property("name", "", ""));
+        properties.add(new Property("geometric_mean_radius", "", "ft"));
         properties.add(new Property("resistance", "", "Ohm/mile"));
-        properties.add(new Property("diameter", "", "inches"));
-        properties.add(new Property("rating.summer.continuous", "", " A"));
+        properties.add(new Property("diameter", "", "in"));
+        properties.add(new Property("rating.summer.continuous", "", "A"));
         properties.add(new Property("rating.summer.emergency", "", "A"));
-        properties.add(new Property("rating.winter.continuous", "", " A"));
+        properties.add(new Property("rating.winter.continuous", "", "A"));
         properties.add(new Property("rating.winter.emergency", "", "A"));
+    }
 
-    }
-    public TriplexLineConductor(TriplexLineConductor newOne)
-    {
-        properties=newOne.GetProperties();
-    }
     public void SetProperty(Vector<Property> wektor){
         properties=wektor;
     }
@@ -33,7 +29,7 @@ public class TriplexLineConductor {
     }
     public String ToGLM(){
         String s="";
-        s="object triplex_line_conductor { \n";
+        s="object overhead_line_conductor { \n";
         for (Property p: properties){
             if(p.GetName()!=null)s+=p.GetName()+"    "+p.GetValue()+"    "+p.GetUnit()+";\n";
         }
