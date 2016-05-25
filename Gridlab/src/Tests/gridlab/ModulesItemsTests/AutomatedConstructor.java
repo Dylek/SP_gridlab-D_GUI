7cf6fa6b338c4566b1e8397e0312c3428ea3a424
Marcin <marcinjaj@gmail.com>
2016-05-25 20:23:12 +0200

    update(AutomatedConstructor) dodałem generowanie kodu metody TestToGLM i TestGetModule

--- Gridlab/src/Tests/gridlab/ModulesItemsTests/AutomatedConstructor.java
+++ Gridlab/src/Tests/gridlab/ModulesItemsTests/AutomatedConstructor.java
@@ -1,5 +1,6 @@
 package Tests.gridlab.ModulesItemsTests;
 
+import gridlab.ModulesItems.Property;
 import gridlab.ModulesItems.Residental.House;
 
 /**
@@ -12,11 +13,41 @@ public class AutomatedConstructor {
         dzięki temu wygeneruje wam kod do TestCOnstructor()
         haha ale przechytrzyłem system :v
          */
-        House h=new House();
+        House h=new House();//to modyfikujesz
+        String className="house";//i to też modyfikujesz
+
+
+
+        System.out.println("\n@Test\n public void testConstructor(){\n");
         for(int i=0;i<h.GetProperties().size();i++){
             System.out.println(" assertEquals(\""+h.GetProperties().elementAt(i).GetName()+"\", objectTest.GetProperties().elementAt("+i+").GetName());\n" +
                     "        assertEquals(\""+h.GetProperties().elementAt(i).GetValue()+"\", objectTest.GetProperties().elementAt("+i+").GetValue());\n" +
                     "        assertEquals(\""+h.GetProperties().elementAt(i).GetUnit()+"\", objectTest.GetProperties().elementAt("+i+").GetUnit());\n");
         }
+
+
+
+        System.out.println("\n\n}");
+
+
+        System.out.println("\n@Test\n" +
+                "    public void testGetModule(){assertEquals("+h.getModule()+",objectTest.getModule());}");
+
+
+        String s;
+        System.out.println("@Test\npublic void testToGLM(){\n");
+        s="assertEquals(\"object "+className+"{ \\n\"+";
+        for (Property p: h.GetProperties()){
+            if(!p.GetValue().isEmpty())
+            {
+                s+=p.GetName()+"    "+p.GetValue();
+                // if(false)s+=" "+p.GetUnit();
+                s+=";\n";
+            }
+        }
+        s+="\n \"\\n}\",";
+        s+="\nobjectTest.ToGLM());";
+        System.out.println(s);
+        System.out.println("\n\n}");
     }
 }
